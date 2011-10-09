1e934aa9fcae7db53011c24610e1c9a20e5d2a0f
Fabian Beiner <mail@fabian-beiner.de>
2011-10-09 23:13:48 +0200

    Trim the numbers and addresses.

--- script.js
+++ script.js
@@ -269,7 +269,8 @@ function phoneClean(number) {
             number = number.replace(re, ' ' + ext);
         }
     }
-    return number;
+    // Return the trimmed number.
+    return number.replace(/^\s+|\s+$/g, '');
 }
 
 /**
@@ -281,8 +282,8 @@ function mailClean(mail) {
     if (mail.search(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i) == -1) {
         return "";
     }
-
-    return mail;
+    // Return the trimmed email address.
+    return mail.replace(/^\s+|\s+$/g, '');
 }
 
 /**
